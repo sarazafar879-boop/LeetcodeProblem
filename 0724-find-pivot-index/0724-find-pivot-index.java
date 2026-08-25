84835cbdbbf5c4eaf6e79f3adb8f76ba53689909
SARA ZAFAR <sarazafar879@gmail.com>
2026-08-25 09:49:54 +0530

    Time: 1 ms (97.53%), Space: 46.7 MB (99.15%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int ts=0;
+        int ls=0;
+        for(int i=0;i<nums.length;i++){
+            ts+=nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            int rs=ts-ls-nums[i];
+            if(ls==rs) return i;
+            ls+=nums[i];
+        }
+        return-1;
+    }
+}
